cfcf951ec5c4225409e4aa8067308292c9e1fece
ejcupcake1234alt <94024247+ejcupcake1234alt@users.noreply.github.com>
2023-06-07 14:04:31 -0400

    Add files via upload

---
+++ mittens.py
@@ -0,0 +1,51 @@
+import time
+h = 2
+n = 1
+print("WHATS THE PASSWORD")
+password = input()
+
+if password == "iLoveCats123":
+    print("you have done it...")
+    time.sleep(2)
+    print("you can now see the")
+    time.sleep(1)
+    print("C")
+    time.sleep(0.3)
+    print("A")
+    time.sleep(0.3)
+    print("T.")
+    time.sleep(2)
+    print(" |\---/|")
+    print(">| o_o |<")
+    print("  \_^_/ ")
+    time.sleep(3)
+    print("this program will self destruct in 3.")
+    time.sleep(1)
+    print("2.")
+    time.sleep(1)
+    print("1.")
+    time.sleep(1)
+    print("0.")
+    time.sleep(0.5)
+    print("this program will now self destuct")
+    time.sleep(1)
+    while True:
+        print(n)
+        n = n + n
+
+else:
+    time.sleep(1)
+    print("the password is incorrect.")
+    print("this program will self destruct in 3.")
+    time.sleep(1)
+    print("2.")
+    time.sleep(1)
+    print("1.")
+    time.sleep(1)
+    print("0.")
+    time.sleep(0.5)
+    print("this program will now self destuct")
+    time.sleep(1)
+    while True:
+        print(n)
+        n = n + n
